e30ac489e1e66fbca2f559d662f390299c108a4a
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-04-08 16:49:51 +0300

    home work 2_1

--- homework2_1/homework.js
+++ homework2_1/homework.js
@@ -1,4 +1,24 @@
-for (i = 0; i < n; i++) {
+// я считаю что 1 член ряда єто 0
 
+n = +prompt();
+a = 1;
+b = 1;
 
-}
+if (n > 2)
+    {
+    document.write(1 , " член ряда равняется - " , a, '<br>');
+    document.write(2,  " член ряда равняется - " , b, '<br>');
+    for (i = 3; i <= n; i++) 
+        {
+        c = a + b;
+        a = b;
+        b = c;
+        document.write(i, " член ряда равняется - " , c, '<br>');
+        }
+    }else
+    {
+        for(i = 1; i <= n; i++)
+        {
+            document.write(i, " член ряда равняется - " , a, '<br>');
+        }
+    }
