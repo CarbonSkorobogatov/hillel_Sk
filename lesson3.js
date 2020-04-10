d56a8413a9affb934499480515ef816841b3f099
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-04-10 20:07:58 +0300

    lesson

--- lesson3.js
+++ lesson3.js
@@ -8,3 +8,30 @@ for(i = 0; i < A.lenght; i++){
     console.log(A[i]);
 }
 
+max = A[0];
+min = A[0];
+sum = 0;
+mul = 1;
+
+for(i = 1; i < A.lenght; i++){
+    if(max < A[i]){
+        max = A[i];
+    }
+    if(A[i] < min){
+        min = A[i];
+    }
+    mul *= A[i];
+    sum += A[i];
+}
+/*
+console.log('max = ', max);
+console.log('min = ', min);
+console.log('sum = ', sum);
+console.log('mul = ', mul);
+*/
+//-----------------------------------------------------------------------------------------//
+A.push(50); //Добавление
+A.pop();
+A.pop();
+console.log(A)
+
