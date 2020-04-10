b49e3d709bb86cfc8ae5c9333a3b179ce7e817c8
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-04-10 19:18:46 +0300

    homework2

--- homework2_2/homework.js
+++ homework2_2/homework.js
@@ -1,6 +1,7 @@
 x = parseInt(prompt("Введите число"));
 y = parseInt(prompt("Введите число, которое больше приведущего"));
 
+sum = 0;
 
 //я считал учитывая, что факториал 0 = 1
 if(x < y && x > 0 && y > 0)
@@ -13,21 +14,28 @@ if(x < y && x > 0 && y > 0)
             factorial = j * factorial;
         }
         document.write("Факториал числа - ", i, "  равняется  ", factorial, "<br>");
+        sum = sum + factorial;
     }
+    document.write("<br>Сума факториалов = ", sum, "<br>");
 }
+
 else{
-    alert("ввод неправильных данных");
+    document.write("ввод неправильных данных");
 }
 
 document.write("*************************************************<br>");
 document.write("Чётные числа от 0 до 100 <br>");
 
+
+
+document.write("<tablet>");
 for(i = 0; i <=100; i++){
     if(i % 2 == 0){
-        document.write(i,"<br>");
+        document.write("<tr>",i,"</tr>");
     }
 
 }
+document.write("</tablet>");
 
 
 
