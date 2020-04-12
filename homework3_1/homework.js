f9ede3da89bc143febf3374a68d92a891d748f6f
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-04-12 13:54:07 +0300

    homework 3_1

--- homework3_1/homework.js
+++ homework3_1/homework.js
@@ -6,4 +6,51 @@ if(n <= 0 || isNaN(n) == true){
         n = parseInt(prompt("Введите размерность одномерного массива. Подсказка n > 0 "));
     }
 }
+A = [];
+A.lenght = n;
+
+B = [];
+B.lenght = n;
+b = 0;
+
+for(i = 0; i < n; i++){
+    A[i] = Math.floor(Math.random() * 101 - 50);
+    console.log(A[i]);
+
+    k = 0;
+    for(j = 2; j < A[i]; j++){
+        if(A[i] % j == 0){
+            k++;
+        }
+    }
+    if(k == 0 && A[i] > 0){
+        B[b] = A[i];
+        b++;
+    }
+}
+
+max = A[0];
+min = A[0];
+for(i = 1; i < A.lenght; i++){
+    if(A[i] > max){
+        max = A[i];
+    }
+    if(A[i] < min){
+        min = A[i];
+    }
+}
+console.log('max = ', max);
+console.log('min = ', min);
+
+
+console.log("---------------------------");
+console.log('B: ');
+B.lenght = b;
+for(i = 0; i < b; i++){
+    console.log(B[i]);
+}
+
+
+
+
 
