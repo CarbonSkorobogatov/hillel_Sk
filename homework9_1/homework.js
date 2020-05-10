d472aae2859dddca5595b42bccc4630203a9190c
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-05-10 13:25:28 +0300

    home91

--- homework9_1/homework.js
+++ homework9_1/homework.js
@@ -1,3 +1,5 @@
+
+
 var input1 = document.querySelector('.input1');
 var input2 = document.querySelector('.input2');
 var input3 = document.querySelector('.input3');
@@ -5,13 +7,5 @@ var textareas = document.querySelector('.idtextarea');
 
 
 setInterval(function(){
-
-    
-    textareas.value = input1 + ',' + input2 + ',' + input3;
+    textareas.value = input1.value + ',' + input2.value + ',' + input3.value;
 }, 2000)
-
-
-
-
-
-
---
+++ lesson10.js
@@ -0,0 +1,4 @@
+console.log('leson css1');
+
+let block = document.getElementById("special");
+console.dir(block);
